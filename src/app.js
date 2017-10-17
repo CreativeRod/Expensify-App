23cae005d75905e9906c10b6995fc37a78b41e65
Creative Roderick <CreativeRoderick@gmail.com>
2017-10-17 16:58:32 -0500

    Added Nav

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -28,20 +28,39 @@ const HelpPage = () => (
   </div>
 );
 
+const NotFoundPage = () => (
+  <div>
+    404! - <Link to="/">Go Home</Link>
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create Expense </NavLink>
+    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+  </header>
+);
+
 // exact={true} is needed to make sure the page does not include the / page with all other routing, only add it to the first one.
+// Switch goes thru your routes one at a time and stops when a page matches, 404 works with a page is not found.
 const routes = (
   <BrowserRouter>
     <div>
-      <Route path="/" component={ ExpenseDashboardPage } exact={true}/>
-      <Route path="/create" component={ AddExpensePage } />
-      <Route path="/edit" component={ EditExpensePage } />
-      <Route path="/help" component={ HelpPage } />
+      <Header />
+      <Switch>
+        <Route path="/" component={ ExpenseDashboardPage } exact={true}/>
+        <Route path="/create" component={ AddExpensePage } />
+        <Route path="/edit" component={ EditExpensePage } />
+        <Route path="/help" component={ HelpPage } />
+        <Route component={ NotFoundPage } />
+      </Switch>
     </div>
+
   </BrowserRouter>
 );
-
-
-
 
 ReactDOM.render(routes, document.getElementById('app'));
 
